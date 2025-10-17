e4faabbbd55076c8b463924bf5c0ca61da88b251
ProgrammerRimon <83152804+Rimon360@users.noreply.github.com>
2025-10-18 01:50:06 +0600

    v1

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
-app.use("/queue", queue)
+app.use("/api/queue", queue)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
